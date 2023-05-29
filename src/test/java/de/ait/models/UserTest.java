5875d37bccd7ea4e275655dd86e52fd97ba1c245
Leonid Usatii <leonid.usatiy@gmail.com>
2023-05-29 13:46:40 +0200

    initial commit

--- src/test/java/de/ait/models/UserTest.java
+++ src/test/java/de/ait/models/UserTest.java
@@ -1,24 +1,33 @@
 package de.ait.models;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
+    User user;
+    @BeforeEach
+    void prepareData() {
+        user = new User("Leonid", "Usatii", 52, 1.78);
+    }
     @org.junit.jupiter.api.Test
     void getLastName() {
-        User user = new User("Leonid", "Usatii", 52, 1.78);
         assertEquals("Leonid", user.getLastName());
     }
 
     @org.junit.jupiter.api.Test
     void getFirstName() {
+        assertEquals("Usatii", user.getFirstName());
     }
 
     @org.junit.jupiter.api.Test
     void getAge() {
+        assertEquals(52, user.getAge());
     }
 
     @org.junit.jupiter.api.Test
     void getHeight() {
+        assertEquals(1.78, user.getHeight());
     }
 }
