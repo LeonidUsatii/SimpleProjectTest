f135620fcd1babe64fc594b3a0539b24a3c861d7
Leonid Usatii <leonid.usatiy@gmail.com>
2023-05-29 15:04:25 +0200

    add 1

--- src/main/java/de/ait/app/Main.java
+++ src/main/java/de/ait/app/Main.java
@@ -1,16 +1,13 @@
 package de.ait.app;
 
-import de.ait.models.User;
 import de.ait.repositories.UsersRepository;
 import de.ait.repositories.UsersRepositoryListImpl;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет");
         UsersRepository usersRepository = new UsersRepositoryListImpl();
-        List<User> users = usersRepository.findAll();
+        //List<User> users = usersRepository.findAll();
 
     }
 }
